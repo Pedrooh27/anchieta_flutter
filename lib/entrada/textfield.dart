ff8fa4cd91673d908b5d981a67eb34c442b924f2
Pedrooh27 <pedropephsilva280@gmail.com>
2026-03-22 21:54:41 -0300

    add buttons and actions

--- lib/entrada/textfield.dart
+++ lib/entrada/textfield.dart
@@ -25,8 +25,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   // final TextEditingController _dateController = TextEditingController();
-  final TextEditingController _controller = TextEditingController();
-  String _nomeUsuario = "";
 
   @override
   Widget build(BuildContext context) {
